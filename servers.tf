17b4624dca724e79489fefc7e401cc547bb11d15
jandersen770 <jandersen770@gmail.com>
2020-08-31 15:06:20 -0400

    Explicate key_name

--- servers.tf
+++ servers.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "rearc-quest-terraform" {
   ami = "ami-0ff8a91507f77f867"
   instance_type = "t2.micro"
-  key_name = var.ami_key_pair_name
+  key_name = "myKeyPair"
   security_groups = [aws_security_group.rearc-quest-terraform-sg.id]
-  subnet_id = aws_subnet.subnet-uno.id
+  subnet_id = aws_subnet.rearc-quest-terraform-subnet.id
 }
