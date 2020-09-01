222481abdafb4e1414495bb1a1d5b5362962ba98
jandersen770 <jandersen770@gmail.com>
2020-09-01 13:28:01 -0400

    Rename script to user_data

--- servers.tf
+++ servers.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "rearc-quest-terraform" {
   ami = "ami-0ff8a91507f77f867"
   instance_type = "t2.micro"
   key_name = "myKeyPair"
-  user_data = "${file("script.sh")}"
+  user_data = "${file("user_data.sh")}"
   security_groups = [aws_security_group.rearc-quest-terraform-sg.id]
   subnet_id = aws_subnet.rearc-quest-terraform-subnet.id
 }
