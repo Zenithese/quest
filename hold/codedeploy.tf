d2e12c3da54e13fd33f5be058fecdb5b2327505a
jandersen770 <jandersen770@gmail.com>
2020-09-02 09:27:15 -0400

    Remove depreicated code

--- hold/codedeploy.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_codedeploy_app" "rearc-quest-terraform-app" {
-  compute_platform = "Server"
-  name             = "rearc-quest-terraform-app"
-}
--- hold/repository.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "rearc-quest-terraform-ecr" {
-  name = "rearc-quest-terraform-ecr"
-}
