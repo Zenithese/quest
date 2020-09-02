7977cd8e0768ff0cc408983ab94e93469911c002
jandersen770 <jandersen770@gmail.com>
2020-09-02 11:13:58 -0400

    Use variable for kep pair pem

--- servers.tf
+++ servers.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "rearc-quest-terraform" {
   ami = "ami-0ff8a91507f77f867"
   instance_type = "t2.micro"
-  key_name = "myKeyPair"
+  key_name = "${var.ami_key_pair_name}"
   user_data = "${file("user_data.sh")}"
   security_groups = [aws_security_group.rearc-quest-terraform-sg.id]
   subnet_id = aws_subnet.rearc-quest-terraform-subnet.id
